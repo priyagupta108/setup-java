e74cc5cb8ca6a4346b8007f7097c01d29aa30021
Priyagupta108 <priyagupta108@github.com>
2025-06-20 17:03:20 +0530

    TEST

--- __tests__/distributors/jetbrains-installer.test.ts
+++ __tests__/distributors/jetbrains-installer.test.ts
@@ -55,9 +55,7 @@ describe('getAvailableVersions', () => {
     expect(availableVersions).not.toBeNull();
 
     const length =
-      os.platform() === 'win32'
-        ? manifestData.length - 1
-        : manifestData.length + 1;
+      os.platform() === 'win32' ? manifestData.length : manifestData.length + 2;
     expect(availableVersions.length).toBe(length);
   }, 10_000);
 });
